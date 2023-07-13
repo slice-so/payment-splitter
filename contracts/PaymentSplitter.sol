ec541efc845307f1745ca399a5804160e2b112e6
Dom-Mac <domenicochad@gmail.com>
2023-07-13 17:48:50 +0200

    refactor

--- contracts/PaymentSplitter.sol
+++ contracts/PaymentSplitter.sol
@@ -9,141 +9,139 @@ import "@openzeppelin/token/ERC20/IERC20.sol";
 /// @author Dom-Mac <@zerohex_eth>
 /// @notice Additional description
 contract PaymentSplitter is ERC1155, IPaymentSplitter {
-
-  //*********************************************************************//
-  // ---------------------------- storage ------------------------------ //
-  //*********************************************************************//
-  struct Payment {
-    bool isPaid;
-    address token;
-    address receiver;
-    uint256 amount;
-    uint256 paidAmount;
-  }
-
-  mapping(uint256 => Payment) public payments;
-  uint256 public paymentCount = 1;
-  
-  constructor() ERC1155("https://") {
-  }
-
-  //*********************************************************************//
-  // ---------------------------- external ----------------------------- //
-  //*********************************************************************//
-
-  function createPaymentSplitter(address token_, address receiver_, uint256 amount_) external {
-    if (amount_ == 0) {
-      revert AmountNotValid();
-    }
-    if (receiver_ == address(0)) {
-      revert ReceiverNotValid();
+    //*********************************************************************//
+    // ---------------------------- storage ------------------------------ //
+    //*********************************************************************//
+    struct Payment {
+        bool isPaid;
+        address token;
+        address receiver;
+        uint256 amount;
+        uint256 paidAmount;
     }
 
-    payments[paymentCount] = Payment(false, token_, receiver_, amount_, 0);
-    ++paymentCount;
+    mapping(uint256 => Payment) public payments;
+    uint256 public paymentCount = 1;
 
-    emit PaymentSplitterCreated(paymentCount, token_, receiver_, amount_, msg.sender);
-  }
+    constructor() ERC1155("https://") {}
 
-  function contributeToPayment(uint256 paymentId_, uint256 amount_) external payable {
-    Payment memory payment = payments[paymentId_];
+    //*********************************************************************//
+    // ---------------------------- external ----------------------------- //
+    //*********************************************************************//
 
-    _beforeContribute(payment, amount_);
+    function createPaymentSplitter(address token_, address receiver_, uint256 amount_) external {
+        if (amount_ == 0) {
+            revert AmountNotValid();
+        }
+        if (receiver_ == address(0)) {
+            revert ReceiverNotValid();
+        }
 
-    payment.paidAmount += amount_;
+        payments[paymentCount] = Payment(false, token_, receiver_, amount_, 0);
+        ++paymentCount;
 
-    payments[paymentId_] = payment;
-
-    _mint(msg.sender, paymentId_, amount_, "");
-
-    if (payment.token == address(0)) {
-      if (msg.value != amount_) {
-        revert AmountNotValid();
-      }
-    } else {
-      if (msg.value != 0) {
-        revert AmountNotValid();
-      }
-      bool success = IERC20(payment.token).transferFrom(msg.sender, address(this), amount_);
-
-      if (!success || payment.paidAmount + amount_ > payment.amount) {
-        revert AmountNotValid(); // TODO: better error handling
-      }
+        emit PaymentSplitterCreated(paymentCount, token_, receiver_, amount_, msg.sender);
     }
 
-    emit Deposit(msg.sender, paymentId_, amount_);
-  }
+    function contributeToPayment(uint256 paymentId_, uint256 amount_) external payable {
+        Payment memory payment = payments[paymentId_];
 
-  function executePayment(uint256 paymentId_) external {
-    Payment memory payment = payments[paymentId_];
+        _beforeContribute(payment, amount_);
 
-    if (payment.paidAmount != payment.amount) {
-      revert AmountNotValid();
+        payment.paidAmount += amount_;
+
+        payments[paymentId_] = payment;
+
+        _mint(msg.sender, paymentId_, amount_, "");
+
+        if (payment.token == address(0)) {
+            if (msg.value != amount_) {
+                revert AmountNotValid();
+            }
+        } else {
+            if (msg.value != 0) {
+                revert AmountNotValid();
+            }
+            bool success = IERC20(payment.token).transferFrom(msg.sender, address(this), amount_);
+
+            if (!success || payment.paidAmount + amount_ > payment.amount) {
+                revert AmountNotValid(); // TODO: better error handling
+            }
+        }
+
+        emit Deposit(msg.sender, paymentId_, amount_);
     }
 
-    if (payment.isPaid) {
-      revert PaymentAlreadyPaid();
+    function executePayment(uint256 paymentId_) external {
+        Payment memory payment = payments[paymentId_];
+
+        if (payment.paidAmount != payment.amount) {
+            revert AmountNotValid();
+        }
+
+        if (payment.isPaid) {
+            revert PaymentAlreadyPaid();
+        }
+
+        payments[paymentId_].isPaid = true;
+
+        if (payment.token == address(0)) {
+            payable(payment.receiver).transfer(payment.amount);
+        } else {
+            bool success = IERC20(payment.token).transfer(payment.receiver, payment.amount);
+
+            if (!success) {
+                revert AmountNotValid(); // TODO: better error handling
+            }
+        }
     }
 
-    payments[paymentId_].isPaid = true;
+    function redeem(uint256 paymentId_, uint256 amount_) external {
+        Payment memory payment = payments[paymentId_];
 
-    if (payment.token == address(0)) {
-      payable(payment.receiver).transfer(payment.amount);
-    } else {
-      bool success = IERC20(payment.token).transfer(payment.receiver, payment.amount);
+        if (payment.isPaid) {
+            revert PaymentAlreadyPaid();
+        }
 
-      if (!success) {
-        revert AmountNotValid(); // TODO: better error handling
-      }
-    }
-  }
+        uint256 balance = balanceOf(msg.sender, paymentId_);
 
-  function redeem(uint256 paymentId_, uint256 amount_) external {
-    Payment memory payment = payments[paymentId_];
+        if (balance < amount_) {
+            revert AmountNotValid();
+        }
 
-    if (payment.isPaid) {
-      revert PaymentAlreadyPaid();
-    }
+        _burn(msg.sender, paymentId_, amount_);
+        payments[paymentId_].paidAmount -= amount_;
 
-    uint256 balance = balanceOf(msg.sender, paymentId_);
+        if (payment.token == address(0)) {
+            payable(msg.sender).transfer(amount_);
+        } else {
+            bool success = IERC20(payment.token).transfer(msg.sender, amount_);
 
-    if (balance < amount_) {
-      revert AmountNotValid();
-    }
-
-    _burn(msg.sender, paymentId_, amount_);
-    payments[paymentId_].paidAmount -= amount_;
-
-    if (payment.token == address(0)) {
-      payable(msg.sender).transfer(amount_);
-    } else {
-      bool success = IERC20(payment.token).transfer(msg.sender, amount_);
-
-      if (!success) {
-        revert AmountNotValid(); // TODO: better error handling
-      }
-    }
-  }
-
-  //*********************************************************************//
-  // ---------------------------- internal ----------------------------- //
-  //*********************************************************************//
-
-  function _beforeContribute(Payment memory payment_, uint256 amount_) internal pure {
-      if (payment_.amount == 0) {
-      revert PaymentNotExists();
+            if (!success) {
+                revert AmountNotValid(); // TODO: better error handling
+            }
+        }
     }
 
-    if (payment_.isPaid) {
-      revert PaymentAlreadyPaid();
-    }
+    //*********************************************************************//
+    // ---------------------------- internal ----------------------------- //
+    //*********************************************************************//
 
-    if (amount_ == 0) {
-      revert AmountNotValid();
-    }
+    function _beforeContribute(Payment memory payment_, uint256 amount_) internal pure {
+        if (payment_.amount == 0) {
+            revert PaymentNotExists();
+        }
 
-    if (amount_ > payment_.amount - payment_.paidAmount) {
-      revert AmountNotValid();
+        if (payment_.isPaid) {
+            revert PaymentAlreadyPaid();
+        }
+
+        if (amount_ == 0) {
+            revert AmountNotValid();
+        }
+
+        if (amount_ > payment_.amount - payment_.paidAmount) {
+            revert AmountNotValid();
+        }
     }
-  }
 }
--- contracts/interfaces/IPaymentSplitter.sol
+++ contracts/interfaces/IPaymentSplitter.sol
@@ -2,17 +2,27 @@
 pragma solidity ^0.8;
 
 interface IPaymentSplitter {
-  //*********************************************************************//
-  // ---------------------------- errors ------------------------------- //
-  //*********************************************************************//
-  error AmountNotValid();
-  error ReceiverNotValid();
-  error PaymentAlreadyPaid();
-  error PaymentNotExists();
+    //*********************************************************************//
+    // ---------------------------- errors ------------------------------- //
+    //*********************************************************************//
+    error AmountNotValid();
+    error ReceiverNotValid();
+    error PaymentAlreadyPaid();
+    error PaymentNotExists();
 
-  //*********************************************************************//
-  // ---------------------------- events ------------------------------- //
-  //*********************************************************************//
-  event PaymentSplitterCreated(uint256 indexed paymentId_, address token_, address indexed receiver_, uint256 amount_, address indexed creator_);
-  event Deposit(address indexed contributor_, uint256 indexed paymentId_, uint256 amount_);
+    //*********************************************************************//
+    // ---------------------------- events ------------------------------- //
+    //*********************************************************************//
+    event PaymentSplitterCreated(
+        uint256 indexed paymentId_, address token_, address indexed receiver_, uint256 amount_, address indexed creator_
+    );
+    event Deposit(address indexed contributor_, uint256 indexed paymentId_, uint256 amount_);
+
+    //*********************************************************************//
+    // ---------------------------- external ----------------------------- //
+    //*********************************************************************//
+    function createPaymentSplitter(address token_, address receiver_, uint256 amount_) external;
+    function contributeToPayment(uint256 paymentId_, uint256 amount_) external payable;
+    function executePayment(uint256 paymentId_) external;
+    function redeem(uint256 paymentId_, uint256 amount_) external;
 }
