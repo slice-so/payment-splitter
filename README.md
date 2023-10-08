6c00d6172972721f8e8379b60f1acdb869c345a6
thisisommore <ommore501@gmail.com>
2023-10-08 17:39:34 +0000

    Deploy to polygon zkEVM mainnet

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # Foundry template
 
-Contract deployed to Polygon ZKEVM Testnet `0x4e922456030410bA5eF8F172c49598506Ed369Ae`
-https://testnet-zkevm.polygonscan.com/address/0x4e922456030410bA5eF8F172c49598506Ed369Ae
-
-CREATE3 deployed to Polygon ZKEVM Testnet `0x8Ae72A2311A7d225B6CA7B4fdfb355CdBf64E647`
-https://testnet-zkevm.polygonscan.com/address/0x8Ae72A2311A7d225B6CA7B4fdfb355CdBf64E647
+Contract deployed to Polygon ZKEVM `0x0b155941368Fe383FDBA03eB87C612F8a4744Ac3`
+https://testnet-zkevm.polygonscan.com/address/0x0b155941368Fe383FDBA03eB87C612F8a4744Ac3
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,16 +4,19 @@ pragma solidity ^0.8;
 import "forge-std/Script.sol";
 import {CREATE3Factory} from "create3-factory/CREATE3Factory.sol";
 import {PaymentSplitter} from "contracts/PaymentSplitter.sol";
+import "forge-std/console.sol";
 
 contract DeployScript is Script {
-    CREATE3Factory create3Factory = CREATE3Factory(0x8Ae72A2311A7d225B6CA7B4fdfb355CdBf64E647);
-    bytes32 salt = keccak256(bytes(vm.envString("SALT")));
+    // CREATE3Factory create3Factory = CREATE3Factory(0x236b01266D06c2E6a91142548d848732BB70B042);
+    // bytes32 salt = keccak256(bytes(vm.envString("SALT")));
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
     function run() public returns (PaymentSplitter myContract) {
+        // new PaymentSplitter();
         vm.startBroadcast(deployerPrivateKey);
-
-        myContract = PaymentSplitter(create3Factory.deploy(salt, bytes.concat(type(PaymentSplitter).creationCode)));
+        // address ps = create3Factory.deploy(salt, bytes.concat(type(PaymentSplitter).creationCode));
+        // console.log("arare",ps);
+        myContract = new PaymentSplitter();
 
         vm.stopBroadcast();
     }
